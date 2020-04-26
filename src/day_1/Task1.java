64d9a92bc283d674e295d6c0fb74ca733873bc14
Alison Converse <Alison.france.yew@gmail.com>
2020-04-25 20:48:40 -0400

    add Task1.java

---
+++ src/day_1/Task1.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class Task1 {
+
+    //This is how we push our code to Github
+    //create a Class
+    //git add .
+    //git commit -m"add "ClassName.java"
+    //git push
+    //go to github and refresh page
+}
